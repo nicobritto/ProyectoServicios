32a5a7e319b713f1befb63ee3de596790d09c147
nicolas britto <nicobritto21@gmail.com>
2023-05-11 20:45:14 -0300

    proyecto

---
+++ src/main/java/com/ProyectoEgg/EggProyectoServicios/WebServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoEgg.EggProyectoServicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoEgg/EggProyectoServicios/WebServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoEgg.EggProyectoServicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
