7a611b7b770a1292081a9540de13e2b0f892fc85
Tatiana Sanagua <tati_sanagua@hotmail.com>
2023-05-14 19:07:08 -0300

    Creacion de la funcion listar todos los proveedores, creacion de la vista de todos los servicios

--- src/main/java/com/ProyectoEgg/EggProyectoServicios/controller/ControllerProveedor.java
+++ src/main/java/com/ProyectoEgg/EggProyectoServicios/controller/ControllerProveedor.java
@@ -1,6 +1,8 @@
 package com.ProyectoEgg.EggProyectoServicios.controller;
 
+import com.ProyectoEgg.EggProyectoServicios.entidades.Proveedor;
 import com.ProyectoEgg.EggProyectoServicios.service.ServicioProveedor;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,31 +15,38 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
-@RequestMapping("/")//localhost:8080/autor
+@RequestMapping("/")
 public class ControllerProveedor {
     
-     @Autowired
+    @Autowired
     private ServicioProveedor servicioProveedor;
      
-      @GetMapping("/registrar")//localhost:8080/autor/registrar
-        public String registrar() {
+    @GetMapping("/registrar")
+    public String registrar() {
+        return "proveedorForm.html";
+    }
+        
+    @PostMapping("/registro")
+    public String registro(@RequestParam String nombre,@RequestParam String apellido,@RequestParam String email,@RequestParam String telefono,@RequestParam String rubro, MultipartFile archivo,ModelMap modelo){
+        try {
+            servicioProveedor.crearProveedor(nombre,apellido,email,telefono,rubro,archivo);
+            modelo.put("exito"," todo fue un exito :D ");
+            return "index.html";
+        }catch (Exception e) {
+            modelo.put("error", e.getMessage());
             return "proveedorForm.html";
         }
-        
-        @PostMapping("/registro")
-        public String registro(@RequestParam String nombre,@RequestParam String apellido,@RequestParam String email,@RequestParam String telefono,@RequestParam String rubro, MultipartFile archivo,ModelMap modelo){
-            try {
-                servicioProveedor.crearProveedor(nombre,apellido,email,telefono,rubro,archivo);
-                  modelo.put("exito"," todo fue un exito :D ");
-                 return "index.html";
-            } catch (Exception e) {
-                 modelo.put("error", e.getMessage());
-                   return "proveedorForm.html";
-            }
-              
-        }
+    }
     
+    @GetMapping("/proveedores")
+    public String mostrarTodos(ModelMap modelo){
+        List<Proveedor> proveedores = servicioProveedor.listarTodos();
+        
+        modelo.addAttribute("proveedores", proveedores);
+        
+        return "servicios_todos.html";
+    }
            
-
+    
     
 }
