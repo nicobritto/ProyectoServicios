fa4caa257826a524216b795720f8bd4ddb2ad093
Lucas <LUCASMARTINALE123@GMAIL.COM>
2023-05-24 19:48:03 -0300

    crud Usuario terminado (creo que bien) se agregaron las funciones de listar, eliminar, modificar y crear usuarios. Ademas tuve que agregar 2 archivoc html vacios, usuarios_todos(que seria la vista de la lista de usuarios) y usuarioModificar

--- src/main/java/com/ProyectoEgg/EggProyectoServicios/controller/ControllerUsuario.java
+++ src/main/java/com/ProyectoEgg/EggProyectoServicios/controller/ControllerUsuario.java
@@ -1,10 +1,14 @@
 package com.ProyectoEgg.EggProyectoServicios.controller;
 
+import com.ProyectoEgg.EggProyectoServicios.entidades.Proveedor;
+import com.ProyectoEgg.EggProyectoServicios.entidades.Usuario;
 import com.ProyectoEgg.EggProyectoServicios.service.ServicioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -34,6 +38,42 @@ public class ControllerUsuario {
     }
 
     }
+
+    @GetMapping("/usuarios")
+    public String mostrarTodos(ModelMap modelo){
+        List<Usuario> usuarios = servicioUsuario.listarTodos();
+
+        modelo.addAttribute("usuarios", usuarios);
+        return "usuarios_todos.html";
+    }
+
+    @PostMapping("/modificar/{id}")
+    public String actualizacion(@PathVariable String id, @RequestParam String nombre, @RequestParam String apellido,
+                                @RequestParam String email, @RequestParam String password, ModelMap modelo){
+
+        try {
+            servicioUsuario.modificarUsuario(id, nombre, apellido, email, password);
+            modelo.put("Exito", "Usuario modificado con exito");
+            return "index.html";
+        }catch (Exception e){
+            modelo.put("Error", "Error al modificar el usuario");
+            return "usuarioModificar.html";
+        }
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable String id, ModelMap modelo){
+
+        try {
+            servicioUsuario.eliminar(id);
+            return "redirect:../usuarios";
+
+        }catch (Exception e){
+            modelo.put("Error", e.getMessage());
+            return "redirect:../usuarios";
+        }
+    }
+
 
 
 }
--- src/main/java/com/ProyectoEgg/EggProyectoServicios/service/ServicioUsuario.java
+++ src/main/java/com/ProyectoEgg/EggProyectoServicios/service/ServicioUsuario.java
@@ -6,10 +6,15 @@ import com.ProyectoEgg.EggProyectoServicios.excepciones.MiException;
 import com.ProyectoEgg.EggProyectoServicios.repositorios.UsuarioRepositorio;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+
 
 @Service
 public class ServicioUsuario {
@@ -21,7 +26,7 @@ public class ServicioUsuario {
     @Transactional
     public void crearUsuario(String nombre,String apellido,String email,String password, String password2) throws Exception{
         
-        validar(nombre,apellido,email, password,  password2);
+        validar(nombre,apellido,email, password);
         
         Usuario usuario = new Usuario();
         usuario.setNombre(nombre);
@@ -43,17 +48,41 @@ public class ServicioUsuario {
     
     public void eliminar(String id){
         
-        Usuario uaurio = usuarioRepositorio.getById(id);
+        Usuario usuario = usuarioRepositorio.getById(id);
         
-        usuarioRepositorio.delete(uaurio);
+        usuarioRepositorio.delete(usuario);
         
     }
-     
- 
-    
+    @Transactional
+    public List<Usuario> listarTodos(){
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        usuarios = usuarioRepositorio.findAll();
+
+        return usuarios;
+
+    }
+
+    public void modificarUsuario(String id, String nombre, String apellido,String email, String password) throws Exception{
+        validar( nombre, apellido,  email,  password);
+        Optional<Usuario> respuesta= usuarioRepositorio.findById(id);
+
+        if(respuesta.isPresent()){
+            Usuario usuario = respuesta.get();
+
+            usuario.setNombre(nombre);
+            usuario.setApellido(apellido);
+            usuario.setEmail(email);
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+
+
+            usuarioRepositorio.save(usuario);
+        }
+    }
 
     
-    public void validar(String nombre,String apellido,String email,String password, String password2) throws MiException {
+    public void validar(String nombre,String apellido,String email,String password) throws MiException {
       
         if (nombre.trim().isEmpty()) {
             throw new MiException("Nombre no puede ser nulo");
@@ -68,9 +97,9 @@ public class ServicioUsuario {
         if (password.isEmpty() || password == null || password.length() <= 5) {
             throw new MiException("La contraseña no puede estar vacía, y debe tener más de 5 dígitos");
         }
-        if (!password.equals(password2)) {
-            throw new MiException("Las contraseñas ingresadas deben ser iguales");
-        }
+        //if (!password.equals(password2)) {
+        //    throw new MiException("Las contraseñas ingresadas deben ser iguales");
+        //}
          
           }
     
