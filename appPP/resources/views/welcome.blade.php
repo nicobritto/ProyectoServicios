86bb5d1303f8b4451ce4d9e667179d61a55cbf6d
nicolas britto <nicobritto21@gmail.com>
2023-10-05 13:26:56 -0300

    iniciando con larabel

---
+++ appPP/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/plantilla')
+
+
+@section('ProyectoMarcoSecretaria','Proyecto marco para Secretaria')
+
+@section('contenido')
+     <div class="row">
+         <h1> Arancando con secretaria que saldra nadie lo sabe </h1>
+         <i class="fa fa-wheelchair-alt" aria-hidden="true"></i>
+         <i class="fa fa-address-book" aria-hidden="true"></i>
+
+
+     </div>
+@endsection
